e6eb6e7d2d7c06ea830d5fdc627ac5842c975446
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2018-04-07 13:56:40 +1000

    Add box_layout_demo content (Prac 6 exercise 1)

--- prac_06/KivyDemos-master/box_layout_demo.py
+++ prac_06/KivyDemos-master/box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print('Greet')
+        user_name = self.root.ids.user_name.text
+        self.root.ids.output_label.text = "Hello {}".format(user_name)
+
+    def clear(self):
+        self.root.ids.user_name.text = ""
+        self.root.ids.output_label.text = ""
+
 
 BoxLayoutDemo().run()
